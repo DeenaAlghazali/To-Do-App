ee0d59193957bf6cf71ca5473ac35400bbe79e76
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-04 22:45:26 +0200

    handle error

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,7 +1,10 @@
 const allData = require('./allData');
 const addNote = require('./addNote');
+const { error404, error500 } = require('./error');
 
 module.exports = {
   allData,
   addNote,
+  error404,
+  error500,
 };
--- src/router/router.js
+++ src/router/router.js
@@ -5,4 +5,7 @@ const router = express.Router();
 
 router.get('/allData', controller.allData);
 
+// router.use(controller.error404);
+// router.use(controller.error500);
+
 module.exports = router;
