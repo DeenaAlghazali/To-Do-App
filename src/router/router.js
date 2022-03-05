20b9e1f8ee3434fe2f4947f075a8f923ad04795d
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-06 01:16:09 +0200

    delete from back

--- src/router/router.js
+++ src/router/router.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/allData', controller.allData);
 router.get('/getUsers', controller.getUsers);
 router.post('/create-note', controller.addNote);
+router.delete('/delete-note/:id', controller.deleteNote);
 
 // router.use(controller.error404);
 // router.use(controller.error500);
