facde4b57abdb757413e74369e2ad860fbc4ccc4
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-05 14:35:31 +0200

    display users in back

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,10 +1,12 @@
 const allData = require('./allData');
 const addNote = require('./addNote');
+const getUsers = require('./getUsers');
 const { error404, error500 } = require('./error');
 
 module.exports = {
   allData,
   addNote,
+  getUsers,
   error404,
   error500,
 };
