0eab0e0b3e4ebe3cdf8f38fd38c74512aec02b08
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-06 00:29:44 +0200

    edit insert into database

--- src/controller/addNote.js
+++ src/controller/addNote.js
@@ -1,8 +1,9 @@
 const insertData = require('../database/queries/insertData');
 
 const addNote = (req, res) => {
-  const { title, user_id } = req.body;
-  insertData(title, user_id)
+  const { user_id, note } = req.body;
+  console.log('req.body ', req.body);
+  insertData(user_id, note)
     .then(() => res.redirect('/'))
     .catch((error) => console.log(error));
 };
